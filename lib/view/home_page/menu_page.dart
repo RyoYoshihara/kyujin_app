c14e47327c0518a8af43637fecd09f3f768835b8
Ryo Yoshihara <>
2024-06-20 16:10:29 +0900

    プロフィールページ、メニューページのAppBar設定追加

--- lib/view/home_page/menu_page.dart
+++ lib/view/home_page/menu_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,6 +6,24 @@ class MenuPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 60,
+        centerTitle: true,
+        title: const Text(
+            'その他',
+            style: TextStyle(fontWeight:FontWeight.bold,color: Colors.white)
+        ),
+        backgroundColor: Colors.black,
+        bottom: const PreferredSize(
+          preferredSize: Size.fromHeight(1.0), // Dividerの高さを設定
+          child: Divider(
+            height: 1.0, // Dividerの高さを設定
+            thickness: 1.0, // Dividerの厚さを設定
+            color: Colors.grey, // Dividerの色を設定
+          ),
+        ),
+      ),
+    );
   }
 }
